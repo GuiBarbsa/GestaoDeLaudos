80afd58ee3bdeaeec4b25e1222f4bdc999083e50
Guilherme Barbosa <guibarbosa0208@gmail.com>
2023-11-25 13:59:31 -0300

    Validando as autenticações de login no arquivo login-script.js e login.html com funçoes em javascript

--- login/login-script.js
+++ login/login-script.js
@@ -1,10 +1,19 @@
 //O DOM vai esperar todo o conteúdo HTML ser criado para funcionar corretamente
-document.addEventListener('DOMContentLoaded', function(){
-  
+document.addEventListener("DOMContentLoaded", function () {
   //definindo uma variável para o evento do Icone (ION-ICON)
-  var ionIcon = document.getElementById('icon')
+  var ionIcon = document.getElementById("icon")
 
   ionIcon.onclick = function () {
-    alert("Se já possui um usuário acesse com suas credenciais. Mas se não tiver clique em 'Cadastre-se'!")
+    alert(
+      "Se já possui um usuário acesse com suas credenciais. Mas se não tiver clique em 'Cadastre-se'!"
+    )
   }
-});
+})
+//Criando as funções para validação do login
+function validateUser() {
+  /*Pegar o valor do campo usuário*/
+  const user = document.getElementById("user").value
+  if (!user) {
+    alert("Insira o nome de usuário para o acesso!")
+  }
+}
